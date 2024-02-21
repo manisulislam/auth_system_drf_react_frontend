f37e84b0dd689866ccd289c497335a247c5ae8a2
anisul islam <anisulislam980340@gmail.com>
2024-02-21 09:37:31 +0600

    authentication completed with email, and others

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
-
+import axiosInstance from '../utlils/axiosInstance';
+import {toast} from 'react-toastify'
 
 
 const Profile = () => {
@@ -8,17 +9,42 @@ const Profile = () => {
   const user = JSON.parse(localStorage.getItem("user"))
   const jwt_access_token = localStorage.getItem("access")
 
+  
+  const getSomeData = async ()=>{
+    const res=await axiosInstance.get("/auth/test-auth/")
+    if (res.status===200) {
+      console.log(res.data)
+    }
+  }
   useEffect(() => {
     if (jwt_access_token === null && !user) {
       navigate("/login")
+    }else{
+      getSomeData()
     }
-  }, [])
+  }, [jwt_access_token,user])
+
+  const handleLogout=async ()=>{
+    const refresh=JSON.parse(localStorage.getItem("refresh"))
+    const res = await axiosInstance.post("/auth/logout/",{"refresh_token":refresh})
+    if (res.status === 200) {
+      localStorage.removeItem("user")
+      localStorage.removeItem("access")
+      localStorage.removeItem("refresh")
+      navigate("/login")
+      toast.success("Logout Successful")
+    }
+
+  }
+
+
+ 
   return (
     <>
       <div className="text-center my-5">
         <h1>Hi, {user && user.names}</h1>
         <p>Welcome to the Profile page</p>
-        <button className="bg-red-500 hover:bg-red-600 my-8 text-white font-semibold py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50 transform hover:scale-105">Logout
+        <button onClick={handleLogout} className="bg-red-500 hover:bg-red-600 my-8 text-white font-semibold py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50 transform hover:scale-105">Logout
         </button>
       </div>
 
---
+++ src/utlils/axiosInstance.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+import dayjs from 'dayjs';
+
+
+
+const token = localStorage.getItem("access") ? JSON.parse(localStorage.getItem("access")) : ""
+const refresh_token = localStorage.getItem("refresh") ? JSON.parse(localStorage.getItem("refresh")) : ""
+
+
+const baseUrl = "http://localhost:8000/api/v1"
+
+const axiosInstance = axios.create({
+    baseURL: baseUrl,
+    "content-type": "application/json",
+    headers: {
+        Authorization: localStorage.getItem("access") ? `Bearer ${token}` : null
+    }
+})
+
+axiosInstance.interceptors.request.use(async req => {
+    if (token) {
+        req.headers.Authorization = `Bearer ${token}`
+        const user = jwtDecode(token)
+        const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1
+        if (!isExpired) {
+            return req
+        }
+        else {
+            const res = await axios.post(`${baseUrl}/auth/token/refresh/`, { refresh: refresh_token })
+            console.log(res.data)
+            if (res.status === 200) {
+                localStorage.setItem("access", JSON.stringify(res.data.access))
+                req.headers.Authorization = `Bearer ${res.data.access}`
+                return req
+
+            } else {
+                const res = await axios.post(`${baseUrl}/auth/logout/`, { "refresh_token": refresh_token })
+                if (res.status === 200) {
+                    localStorage.removeItem("user")
+                    localStorage.removeItem("access")
+                    localStorage.removeItem("refresh")
+                    
+                }
+            }
+        }
+        return req
+    }
+})
+
+export default axiosInstance;
